c01ce816975f4bc5170cf60a1c5a86344bd77202
zrma <bulbitain@gmail.com>
2020-06-29 01:02:39 +0900

    process one more step of tests

--- src/main/scala/BaseConversion.scala
+++ src/main/scala/BaseConversion.scala
@@ -1,3 +1,12 @@
+import scala.annotation.tailrec
+
 object BaseConversion {
-  def decimalToBinary(x: Decimal) = Binary("1100011")
+  def decimalToBinary(decimal: Decimal): Binary = {
+    Binary(toBinary(BigInt(decimal.number), ""))
+  }
+
+  @tailrec private def toBinary(num: BigInt, acc: String): String = {
+    if (num < 2) num.toString + acc
+    else toBinary(num / 2, (num mod 2).toString ++ acc)
+  }
 }
--- src/test/scala/DecimalBinarySpec.scala
+++ src/test/scala/DecimalBinarySpec.scala
@@ -5,6 +5,9 @@ class DecimalBinarySpec extends AnyFlatSpec {
     var binary: Binary = BaseConversion.decimalToBinary(Decimal("99"))
     assert(binary.number == "1100011")
   }
-  it should "convert a number 245 into a binary number 11110101" in pending
+  it should "convert a number 245 into a binary number 11110101" in {
+    var binary: Binary = BaseConversion.decimalToBinary(Decimal("245"))
+    assert(binary.number == "11110101")
+  }
   it should "convert a number 3141 into a binary number 110001000101" in pending
 }
