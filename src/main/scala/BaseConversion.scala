0f9c5c020aca9ba1dd1c5a2a98f2ce3e80532007
zrma <bulbitain@gmail.com>
2020-06-30 22:53:29 +0900

    solve BinaryToDecimalSpec

--- src/main/scala/BaseConversion.scala
+++ src/main/scala/BaseConversion.scala
@@ -5,12 +5,16 @@ object BaseConversion {
     Binary(toBinary(BigInt(decimal.number), ""))
   }
 
+  def binaryToDecimal(binary: Binary): Decimal = {
+    Decimal(toDecimal(binary.number))
+  }
+
   @tailrec private def toBinary(num: BigInt, acc: String): String = {
-    if (num < 2) {
-      num.toString + acc
-    }
-    else {
-      toBinary(num / 2, (num mod 2).toString ++ acc)
-    }
+    if (num < 2) num.toString + acc
+    else toBinary(num / 2, (num mod 2).toString ++ acc)
+  }
+
+  private def toDecimal(num: String): String = {
+    num.map(_.asDigit).reduceLeft(_ * 2 + _).toString
   }
 }
---
+++ src/test/scala/BinaryToDecimalSpec.scala
@@ -0,0 +1,18 @@
+import org.scalatest.flatspec.AnyFlatSpec
+
+class BinaryToDecimalSpec extends AnyFlatSpec {
+  "base conversion utility" should "convert binary number 100100111101 to decimal equivalent 2365" in {
+    var decimal: Decimal = BaseConversion.binaryToDecimal(Binary("100100111101"))
+    assert(decimal.number == "2365")
+  }
+
+  it should "convert binary number 11110001111110111 to decimal equivalent 123895" in {
+    var decimal: Decimal = BaseConversion.binaryToDecimal(Binary("11110001111110111"))
+    assert(decimal.number == "123895")
+  }
+
+  it should "convert binary number 100000000000001110000001 to decimal equivalent 8389505" in {
+    var decimal: Decimal = BaseConversion.binaryToDecimal(Binary("100000000000001110000001"))
+    assert(decimal.number == "8389505")
+  }
+}
